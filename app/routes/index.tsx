e195139238b61c20c66746c746dd56bb57522864
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2022-12-29 21:04:22 +0700

    input new message

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,24 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import createServerSupabase from "utils/supabase.server";
 
 import Login from "components/login";
 
 import type { LoaderArgs } from "@remix-run/node";
+
+export const action = async ({ request }: LoaderArgs) => {
+  const response = new Response();
+  const supabase = createServerSupabase({ request, response });
+
+  const { message } = Object.fromEntries(await request.formData());
+
+  const { error } = await supabase.from("messages").insert({ content: String(message) });
+
+  if (error) {
+    console.log(error);
+  }
+  return json(null, { headers: request.headers });
+};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response();
@@ -21,6 +35,10 @@ export default function Index() {
     <main>
       <Login />
       <pre>{JSON.stringify(messages, null, 2)}</pre>
+      <Form method="post">
+        <input type="text" name="message" />
+        <button type="submit">send</button>
+      </Form>
     </main>
   );
 }
