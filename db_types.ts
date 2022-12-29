b41442c465caaac329ae363bdd5d55af82118613
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2022-12-29 20:48:49 +0700

    fixing db_types

--- db_types.ts
+++ db_types.ts
@@ -1,83 +1,37 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
   public: {
     Tables: {
       messages: {
         Row: {
-          id: string
-          created_at: string
-          content: string
-        }
+          id: string;
+          created_at: string;
+          content: string;
+          user_id: string | null;
+        };
         Insert: {
-          id?: string
-          created_at?: string
-          content: string
-        }
+          id?: string;
+          created_at?: string;
+          content: string;
+          user_id?: string | null;
+        };
         Update: {
-          id?: string
-          created_at?: string
-          content?: string
-        }
-      }
-    }
+          id?: string;
+          created_at?: string;
+          content?: string;
+          user_id?: string | null;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
-  }
-}
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
-
-export interface Database {
-  public: {
-    Tables: {
-      messages: {
-        Row: {
-          id: string
-          created_at: string
-          content: string
-          user_id: string | null
-        }
-        Insert: {
-          id?: string
-          created_at?: string
-          content: string
-          user_id?: string | null
-        }
-        Update: {
-          id?: string
-          created_at?: string
-          content?: string
-          user_id?: string | null
-        }
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
